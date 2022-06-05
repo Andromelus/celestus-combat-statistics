2493b1100c651c21977d39848c45aeb028e0bb6f
Florian CASTELAIN <castelainflorian44@gmail.com>
2022-06-05 11:12:12 +0200

    feat: handle list and html output

--- run.sh
+++ run.sh
@@ -4,6 +4,13 @@ echo "Calculator version 2.0.0"
 
 nbr_reports=$(ls -l ./reports/*.htm | wc -l)
 
+if [[ -z "$1" ]]; then
+    echo "notice: no output style set. Stats will be displayed as list."
+    output_style=""
+else
+    output_style="$1"
+fi
+
 if [[ $nbr_reports -eq 1 ]]; then
     if ! awk -f ./src/stats.awk ./reports/*.htm >> tmp_stats ; then
         tail -1 tmp_stats
@@ -17,5 +24,5 @@ else
         fi
     done
 fi
-awk -f ./src/calculated_stats.awk tmp_stats
+awk -f ./src/calculated_stats.awk -v output_style="$output_style" tmp_stats
 rm tmp_stats
